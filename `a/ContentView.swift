bfd1bd808ef8f1d5d955add1164cf409adae2448
EvgeniyKuropatkin <kuropatkin-evgeniy@mail.ru>
2025-09-10 21:30:56 +0300

    git init

---
+++ `a/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  `a
+//
+//  Created by 1111 on 04.09.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack (alignment: .leading){
+            Text("Hello, world!")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ `a/_aApp.swift
@@ -0,0 +1,17 @@
+//
+//  _aApp.swift
+//  `a
+//
+//  Created by 1111 on 04.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct _aApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
